d412dee028dc51fa070e1caf9950c8d2a297b543
Noah Ryan Gochenauer <nrg5391@psu.edu>
2026-04-23 13:38:48 -0400

    Third commit

--- Final.py
+++ Final.py
@@ -10,3 +10,18 @@ CELL_SIZE = WIDTH // 3
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic-Tac-Toe")
 
+BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+X_COLOR = (66, 66, 66)
+O_COLOR = (239, 231, 200)
+
+board = [["" for _ in range(3)] for _ in range(3)]
+current_player = "X"
+game_over = False
+
+def draw_lines():
+    pygame.draw.line(screen, LINE_COLOR, (0, 100), (300, 100), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (300, 200), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (100, 0), (100, 300), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 300), LINE_WIDTH)
+
