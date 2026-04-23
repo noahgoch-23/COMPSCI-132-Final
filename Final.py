ae1041fcb7dcf2e765eef6fc578c51bb9b520485
Noah Ryan Gochenauer <nrg5391@psu.edu>
2026-04-23 13:31:20 -0400

    Second commit

--- Final.py
+++ Final.py
@@ -2,3 +2,11 @@ import pygame
 import sys
 
 pygame.init()
+
+WIDTH, HEIGHT = 300, 300
+LINE_WIDTH = 5
+CELL_SIZE = WIDTH // 3
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic-Tac-Toe")
+
