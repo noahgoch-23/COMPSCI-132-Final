de4833296b2aefaae8f41a4ac223de58e7c653b1
Noah Ryan Gochenauer <nrg5391@psu.edu>
2026-04-26 18:51:09 -0400

    Third commit

--- Final.py
+++ Final.py
@@ -25,3 +25,36 @@ def draw_lines():
     pygame.draw.line(screen, LINE_COLOR, (100, 0), (100, 300), LINE_WIDTH)
     pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 300), LINE_WIDTH)
 
+def draw_figures():
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == "X":
+                pygame.draw.line(screen, X_COLOR,
+                                 (col * CELL_SIZE + 20, row * CELL_SIZE + 20),
+                                 (col * CELL_SIZE + 80, row * CELL_SIZE + 80), 5)
+                pygame.draw.line(screen, X_COLOR,
+                                 (col * CELL_SIZE + 80, row * CELL_SIZE + 20),
+                                 (col * CELL_SIZE + 20, row * CELL_SIZE + 80), 5)
+            
+            elif board[row][col] == "O":
+                pygame.draw.circle(screen, O_COLOR,
+                                   (col * CELL_SIZE + 50, row * CELL_SIZE + 50),
+                                   30, 5)
+                
+def check_winner(player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+        
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+               return True
+        
+    if all(board[i][i] == player for i in range(3)):
+        return True
+    
+    if all(board[i][2 -i] == player for i in range(3)):
+        return True
+    
+    return False
+
